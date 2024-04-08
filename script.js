2918c582d2b2eec2ae34d0b775969c01a7df6a17
M-Ali-Haider <mah01134212100@gmail.com>
2024-04-08 16:15:33 +0500

    changed theme

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const hbm = document.getElementById('hbm');
 const mbe =document.getElementById('mbe');
+let hbml = document.querySelectorAll(".hbm-l");
 let isHbmOpen=false;
 
 hbm.addEventListener('click',()=>{
     if(isHbmOpen){
-        hbm.style.backgroundColor='var(--main-color)';
+        hbm.style.backgroundColor='#fff';
         mbe.style.height='0%';
+        hbml.forEach(function(element) {
+          element.style.backgroundColor = "#005a7e";
+        });
         document.body.style.overflow = 'auto';
     }
     else{
-        hbm.style.backgroundColor='var(--header-button-hover)';
+        hbm.style.backgroundColor='#005a7e';
         mbe.style.height='50%';
+        hbml.forEach(function(element) {
+          element.style.backgroundColor = "#fff";
+        });
         document.body.style.overflow='hidden';
     }
     isHbmOpen=!isHbmOpen;
